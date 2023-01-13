a821f2ed36bfecea7be789d8306b7c7155e7e374
slyphstia <apiwit_boonyarit@cmu.ac.th>
2023-01-13 17:03:52 +0700

    HARD

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,41 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+void stat(const double A[],int N,double B[]){
+  double sum =0;
+  double sx = 0;
+  double geo =1;
+  double har =0;
+
+  
+for(int i = 0;i < N;i++) {
+sum += A[i] ; 
+}
+B[0] = sum/N ;
+for(int i = 0; i < N ;i++){
+sx = pow(A[i] - B[0],2)+sx;
+}
+B[1] = sqrt(sx/N);
+for(int i = 0; i< N ;i++){
+geo *= A[i] ;
+
+}
+B[2]=pow(geo,1.0/N) ;
+
+for(int i = 0; i < N ;i++){
+har +=(1/A[i])  ;
+}
+B[3]=N/har ;
+
+B[4] = A[0];
+B[5] = A[0];
+for(int i = 0;i < N;i++){
+if(B[4]<A[i]){
+B[4]=A[i];
+}
+if(B[5]>A[i]){
+B[5]=A[i];
+}
+}
+}
